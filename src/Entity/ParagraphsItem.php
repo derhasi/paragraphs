13af49b64e94943e5983ed71dd791dbd8e52f1a6
Jeroen <jeroen@vdmi.nl>
2015-01-20 21:50:03 +0100

    Remove vid function

--- src/Entity/ParagraphsItem.php
+++ src/Entity/ParagraphsItem.php
@@ -157,10 +157,6 @@ class ParagraphsItem extends ContentEntityBase implements ParagraphsItemInterfac
     return $this;
   }
 
-  public function vid() {
-    return $this->getEntityKey('vid');
-  }
-
   /**
    * {@inheritdoc}
    */
