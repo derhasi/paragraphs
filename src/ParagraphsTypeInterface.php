a68e735c17eb8d2b4126fe525599c1fbd29f36f7
Jeroen <jeroen@vdmi.nl>
2015-03-02 16:30:41 +0100

    Revert "Fix tests"
    This reverts commit bb7a4f4b4354547dd2c11bdd3fab203b7fb5779f.

--- src/ParagraphsTypeInterface.php
+++ src/ParagraphsTypeInterface.php
@@ -8,12 +8,15 @@
 namespace Drupal\paragraphs;
 
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
+use Drupal\Core\Config\Entity\ThirdPartySettingsInterface;
 
 /**
  * Provides an interface defining a ParagraphsType entity.
  */
-interface ParagraphsTypeInterface extends ConfigEntityInterface
+interface ParagraphsTypeInterface extends ConfigEntityInterface, ThirdPartySettingsInterface
 {
+
   // Add get/set methods for your configuration properties here.
+  
 
 }
