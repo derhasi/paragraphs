b4efdc284fd5a3145a3ac14a7ab2c5afe3d7b979
anushka-mp <anushka-mp@2927677.no-reply.drupal.org>
2015-03-02 16:31:04 +0100

    Issue #2444345 by Anushka-mp: Test failures

--- src/ParagraphsTypeInterface.php
+++ src/ParagraphsTypeInterface.php
@@ -8,15 +8,12 @@
 namespace Drupal\paragraphs;
 
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
-use Drupal\Core\Config\Entity\ThirdPartySettingsInterface;
 
 /**
  * Provides an interface defining a ParagraphsType entity.
  */
-interface ParagraphsTypeInterface extends ConfigEntityInterface, ThirdPartySettingsInterface
+interface ParagraphsTypeInterface extends ConfigEntityInterface
 {
-
   // Add get/set methods for your configuration properties here.
-  
 
 }
