7d70adcb69416f35eb2d2596340461df8efda009
mirodietiker <mirodietiker@227761.no-reply.drupal.org>
2016-07-23 13:51:27 +0200

    Issue #2760341 by yongt9412, miro_dietiker: Let the user be aware that Paragraphs fields are not supported for translation

--- src/Tests/ParagraphsConfigTest.php
+++ src/Tests/ParagraphsConfigTest.php
@@ -60,4 +60,56 @@ class ParagraphsConfigTest extends ParagraphsTestBase {
     $this->drupalPostForm(NULL, [], t('Save and keep published (this translation)'));
     $this->assertText('paragraphed_test paragraphed_title has been updated.');
   }
+
+  /**
+   * Tests content translation form translatability constraints messages.
+   */
+  public function testContentTranslationForm() {
+    $this->loginAsAdmin([
+      'administer languages',
+      'administer content translation',
+      'create content translations',
+      'translate any entity',
+    ]);
+
+    // Check warning message is displayed.
+    $this->drupalGet('admin/config/regional/content-language');
+    $this->assertText('(* unsupported) Paragraphs fields do not support translation.');
+
+    $this->addParagraphedContentType('paragraphed_test', 'paragraphs_field');
+
+    // Check error message is displayed.
+    $this->drupalGet('admin/config/regional/content-language');
+    $this->assertText('(* unsupported) Paragraphs fields do not support translation.');
+    $this->assertRaw('<div class="messages messages--error');
+
+    // Add a second language.
+    ConfigurableLanguage::create(['id' => 'de'])->save();
+
+    // Enable translation for paragraphed content type.
+    $edit = [
+      'entity_types[node]' => TRUE,
+      'settings[node][paragraphed_test][translatable]' => TRUE,
+      'settings[node][paragraphed_test][fields][paragraphs_field]' => FALSE,
+    ];
+    $this->drupalPostForm('admin/config/regional/content-language', $edit, t('Save configuration'));
+
+    // Check content type field management warning.
+    $this->drupalGet('admin/structure/types/manage/paragraphed_test/fields/node.paragraphed_test.paragraphs_field');
+    $this->assertText('Paragraphs fields do not support translation.');
+
+    // Make the paragraphs field translatable.
+    $edit = [
+      'entity_types[node]' => TRUE,
+      'settings[node][paragraphed_test][translatable]' => TRUE,
+      'settings[node][paragraphed_test][fields][paragraphs_field]' => TRUE,
+    ];
+    $this->drupalPostForm('admin/config/regional/content-language', $edit, t('Save configuration'));
+
+    // Check content type field management error.
+    $this->drupalGet('admin/structure/types/manage/paragraphed_test/fields/node.paragraphed_test.paragraphs_field');
+    $this->assertText('Paragraphs fields do not support translation.');
+    $this->assertRaw('<div class="messages messages--error');
+
+  }
 }
